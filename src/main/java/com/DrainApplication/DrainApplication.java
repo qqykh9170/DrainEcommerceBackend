2983e429bd8d867c0ea1f9121146ca71d7021bb6
qqykh9170 <angelgabrielev1907@gmail.com>
2024-12-18 21:43:43 -0500

    First commit

---
+++ src/main/java/com/DrainApplication/DrainApplication.java
@@ -0,0 +1,13 @@
+package com.DrainApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DrainApplication/DrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DrainApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
